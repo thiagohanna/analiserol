4a474491d8128d31fe3f887f6c2c12be855c58e2
thiagohanna <76961758+thiagohanna@users.noreply.github.com>
2024-09-28 01:31:07 -0300

    main2.py

--- main2.py
+++ main2.py
@@ -377,8 +377,8 @@ if st.session_state['legenda']:
 st.markdown("<h3>Painel de Resultados</h3>", unsafe_allow_html=True)
 
 col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-col8, col9, col10, col11, col12, col13, col14, col15 = st.columns(7)
-col16, col17, col18, col19, col20, col21, col22, col23, col24, col25 = st.columns(7)
+col8, col9, col10, col11, col12, col13, col14, col15 = st.columns(8)
+col16, col17, col18, col19, col20, col21, col22, col23, col24, col25 = st.columns(10)
 
 with col1:
     if st.button("Cor"):
