0a789e01a4ac76511a6547ffbb0bd6998b783ba4
thiagohanna <76961758+thiagohanna@users.noreply.github.com>
2024-10-08 11:32:13 -0300

    main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import numpy as np
 def analyze_numbers(number_list):
     counts = {i: {j: 0 for j in range(37)} for i in range(37)}
     total_counts = {i: 0 for i in range(37)}
-    
+
     # Remover números consecutivos repetidos para considerar apenas uma ocorrência
     filtered_list = [number_list[i] for i in range(len(number_list)) if i == 0 or number_list[i] != number_list[i - 1]]
-    
+
     # Percorrer a lista filtrada para contar as ocorrências de pares (y, x)
     for i in range(1, len(filtered_list)):
         y = filtered_list[i - 1]
@@ -17,7 +17,7 @@ def analyze_numbers(number_list):
         if 0 <= y <= 36 and 0 <= x <= 36:  # Verifica se os números estão no intervalo desejado
             counts[x][y] += 1
             total_counts[x] += 1
-    
+
     # Criar uma tabela de análise com os percentuais calculados
     column_names = [f"{i+1}º" for i in range(37)]
     df_analysis = pd.DataFrame(index=range(37), columns=column_names)
@@ -27,15 +27,16 @@ def analyze_numbers(number_list):
             if total_counts[x] > 0:
                 percentage = int((counts[x][y] / total_counts[x]) * 100)  # Remove casas decimais
                 if percentage > 0:  # Apenas mostrar células com valores diferentes de zero
-                    percentages.append((y, f"<span style='font-size:14px;'>{y}</span> <span style='font-size:14px;'>( {percentage}% )</span>"))
+                    percentages.append((y, percentage))
                 else:
-                    percentages.append((y, ""))  # Células vazias para percentuais iguais a zero
+                    percentages.append((y, 0))  # Células vazias para percentuais iguais a zero
             else:
-                percentages.append((y, ""))  # Células vazias se não houver cálculos ativos
+                percentages.append((y, 0))  # Células vazias se não houver cálculos ativos
+
         # Ordenar as células de cada linha pela maior para menor porcentagem
-        percentages.sort(key=lambda item: int(item[1].split('(')[-1][:-2]) if item[1] else 0, reverse=True)
-        df_analysis.loc[x] = [cell[1] for cell in percentages]
-    
+        percentages.sort(key=lambda item: item[1], reverse=True)
+        df_analysis.loc[x] = [f"<span style='font-size:14px;'>{cell[0]}</span> <span style='font-size:14px;'>( {cell[1]}% )</span>" if cell[1] > 0 else "" for cell in percentages]
+
     return df_analysis
 
 # Configuração da página do Streamlit
