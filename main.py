49fdcb2fa1adf42e4182fd73631f0a4ab74430f2
thiagohanna <76961758+thiagohanna@users.noreply.github.com>
2024-10-08 11:06:58 -0300

    main.py

--- main.py
+++ main.py
@@ -22,16 +22,20 @@ def analyze_numbers(number_list):
         for y in range(37):
             if total_counts[x] > 0:
                 percentage = (counts[x][y] / total_counts[x]) * 100
-                percentages.append((y, f"{y} - {percentage:.2f}%"))
+                if percentage > 0:  # Apenas mostrar células com valores diferentes de zero
+                    percentages.append((y, f"{y} - {percentage:.2f}%"))
+                else:
+                    percentages.append((y, ""))  # Células vazias para percentuais iguais a zero
             else:
-                percentages.append((y, f"{y} - 0.00%"))
+                percentages.append((y, ""))  # Células vazias se não houver cálculos ativos
         # Ordenar as células de cada linha pela maior para menor porcentagem
-        percentages.sort(key=lambda item: float(item[1].split(' - ')[1][:-1]), reverse=True)
+        percentages.sort(key=lambda item: float(item[1].split(' - ')[1][:-1]) if item[1] else 0, reverse=True)
         df_analysis.loc[x] = [cell[1] for cell in percentages]
     
     return df_analysis
 
 # Configuração da página do Streamlit
+st.set_page_config(layout="wide")  # Define o layout para tela larga
 st.title("Análise de Números da Roleta")
 st.write("Insira uma lista de números separados por vírgula para analisar a frequência de números à esquerda.")
 
@@ -43,8 +47,8 @@ try:
     number_list = [int(num.strip()) for num in number_input.split(",")]
     df_analysis = analyze_numbers(number_list)
     
-    # Exibir a tabela de análise resultante sem cabeçalhos de coluna
+    # Exibir a tabela de análise resultante sem cabeçalhos de coluna e células sem valores de zero
     st.write("Tabela de Análise de Números da Roleta")
-    st.dataframe(df_analysis.style.hide(axis='columns'))
+    st.dataframe(df_analysis.style.hide(axis='columns'), width=1200, height=1800)
 except ValueError:
     st.error("Por favor, insira apenas números inteiros separados por vírgula.")
